30feb52342572b5c3362d26891d9400bdaf1bca3
Athi Muthu <athidevwork@gmail.com>
2017-07-12 22:17:07 -0400

    Second revision

--- src/main/java/com/springdata/app/SpringdataMongodbApplication.java
+++ src/main/java/com/springdata/app/SpringdataMongodbApplication.java
@@ -1,11 +1,15 @@
 package com.springdata.app;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.google.gson.Gson;
+
 @SpringBootApplication
 public class SpringdataMongodbApplication {
-
+	 static Gson gson = new Gson();
 	 public static void main(String[] args) {
          //SpringApplication.run(SpringdataMongodbApplication.class, args);
+		 String policyJson = args[1];
+		 PolicyInquiryResultType resultType = gson.fromJson(policyJson, PolicyInquiryResultType);
  }
 
 }
